a46b164cd61fa2624e04dda6ef18965452d17cbd
Divyanshukps <divyanshukapse26@gmail.com>
2025-11-08 22:13:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,8 @@ def api_data():
     }
     return jsonify(data)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+
+
